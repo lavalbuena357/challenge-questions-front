9504765d011c905267762b22a0fb11bf516f4e06
cuervo357@gmail.com <cuervo357@gmail.com>
2022-01-05 17:45:11 -0500

    estilos

--- challenge-app/src/App.jsx
+++ challenge-app/src/App.jsx
@@ -3,18 +3,24 @@ import { Routes, Route, Link } from "react-router-dom"
 import Main from './components/Main/Main'
 import Trivia from './pages/Trivia/Trivia'
 import History from './pages/History/History'
+import './App.css'
 
 function App() {
 
   return (
     <div>
-      <h1>Trivia App</h1>
-      <Link to={'/'}>
-        Inicio
-      </Link>
-      <Link to='/history' >
-        Historial de partidas
-      </Link>
+      <div className='header_ctn'>
+        <h1>Trivia App</h1>
+        <div className='links_ctn'>
+          <Link to={'/'}>
+            Inicio
+          </Link>
+          <Link to='/history' >
+            Historial de partidas
+          </Link>
+        </div>
+      </div>
+      
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/trivia/:level_reached' element={<Trivia />} />
