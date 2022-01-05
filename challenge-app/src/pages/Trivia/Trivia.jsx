036fe431d936e8c851779fe402a0c73b51e1c875
cuervo357@gmail.com <cuervo357@gmail.com>
2022-01-05 10:50:41 -0500

    tcomponente trivia funciones y peticiones

--- challenge-app/src/pages/Trivia/Trivia.jsx
+++ challenge-app/src/pages/Trivia/Trivia.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import TriviaCard from '../../components/TriviaCard/TriviaCard'
 import { petition } from '../../utils/petitions'
 import { services } from '../../utils/services'
 
@@ -12,9 +13,12 @@ function Trivia() {
     setUsers(await petition(services.getUsers.url))
   }, [])
 
+  //cargar datos de usuario y llamado a la api del nivel
   useEffect(async () => { // eslint-disable-line
     setUser(users[users.length-1])
-    setLevel(await petition(services.getLevels.url+`/${users[users.length-1] && users[users.length-1].level_reached}`))
+    const petitioLevels = await petition(services.getLevels.url)
+    const currentLevel = await petitioLevels.filter(el => el.level === (users[users.length-1] && users[users.length-1].level_reached))
+    setLevel(currentLevel[0])
     setIsLoading(false)
   }, [users])
 
@@ -23,7 +27,14 @@ function Trivia() {
       {isLoading ?
         <p>Cargando...</p>
         : 
-        <h1>{`Nivel ${level[0] && level[0].level}`}</h1>
+        <div>
+          <h1>{`Nivel ${level && level.level } `}</h1>
+          <div>
+            <span>{`Acumulado: $${user && user.accum}`}</span>
+            <span>{`Premio a ganar: $${level && level.prize.points}`}</span>
+          </div>
+          <TriviaCard level={level} />
+        </div>
       }
     </div>
   )
