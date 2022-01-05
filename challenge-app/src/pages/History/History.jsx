4564a01df3f0e9aab8dc9496d3cd8026f0321f96
cuervo357@gmail.com <cuervo357@gmail.com>
2022-01-05 17:30:18 -0500

    history componet

--- challenge-app/src/pages/History/History.jsx
+++ challenge-app/src/pages/History/History.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {petition} from '../../utils/petitions'
+import { services } from '../../utils/services'
 
 function History() {
+  const [history, setHistory] = useState([])
+
+  useEffect(async () => {
+    const gethistory = await petition(services.getHistory.url, services.getHistory.method)
+    setHistory(gethistory)
+  }, [])
+
   return (
     <div>
-      History
+      <table>
+        <thead>
+          <tr>
+          <th>ID</th>
+          <th>Fecha</th>
+          <th>Nombre</th>
+          <th>Máximo nivel</th>
+          <th>Premio Ganado</th>
+          </tr>
+        </thead>
+        <tbody>
+          {history.length > 0 && history.map((el, i) => (
+            <tr key={i}>
+              <td>{el.id}</td>
+              <td>{el.date.split('T')[0]}</td>
+              <td>{el.user.name}</td>
+              <td>{el.user.level_reached}</td>
+              <td>{el.user.accum}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
