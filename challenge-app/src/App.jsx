83d5faf0b54f060e1fab139086d878c7edd65bd0
cuervo357@gmail.com <cuervo357@gmail.com>
2022-01-04 22:31:12 -0500

    browser router

--- challenge-app/src/App.jsx
+++ challenge-app/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div>
-      App
+      <h1>Trivia App</h1>
+      
     </div>
   );
 }
