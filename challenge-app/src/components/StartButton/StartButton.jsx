d0795b82c5eba168803cc352696a3657e75f551b
cuervo357@gmail.com <cuervo357@gmail.com>
2022-01-05 00:06:59 -0500

    modulo de boton iniciar

--- challenge-app/src/components/StartButton/StartButton.jsx
+++ challenge-app/src/components/StartButton/StartButton.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { petition } from '../../utils/petitions'
+import { services } from '../../utils/services'
 
 function StartButton() {
+
+  const navigate = useNavigate()
+
+  const createUser = async () => {
+    await petition(
+      services.addUser.url, 
+      services.addUser.method, 
+      services.addUser.headers,
+      services.addUser.body
+    )
+    navigate('/trivia/1')
+  }
+
   return (
     <div>
-      <button>Comenzar Juego</button>
+      <button onClick={createUser}>Comenzar Juego</button>
     </div>
   )
 }
