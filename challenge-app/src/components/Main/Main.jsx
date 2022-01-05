1fe9f63e74dac33126586dc5af152e4cb0ed185c
cuervo357@gmail.com <cuervo357@gmail.com>
2022-01-05 16:57:17 -0500

    initial data

--- challenge-app/src/components/Main/Main.jsx
+++ challenge-app/src/components/Main/Main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { petition } from '../../utils/petitions'
+import { services } from '../../utils/services'
 import StartButton from '../StartButton/StartButton'
 
 function Main() {
+  const [initialData, setInitialData] = useState([])
+
+  useEffect( async() => {
+    const users = await petition(services.getUsers.url)
+    setInitialData(users[users.length-1])
+  }, [])
 
   return (
     <div>
+      <span>{`Último premio $${initialData && initialData.accum}`}</span>
       <h2>Responde las preguntas sin fallar y gana el gran acumulado!</h2>
       <StartButton />
     </div>
