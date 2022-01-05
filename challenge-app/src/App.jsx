447d33531e91abccfc825a2092e0a49163baf295
cuervo357@gmail.com <cuervo357@gmail.com>
2022-01-05 00:06:19 -0500

    rutas history y trivia

--- challenge-app/src/App.jsx
+++ challenge-app/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom"
-import Main from './components/Main/Main';
+import Main from './components/Main/Main'
+import Trivia from './pages/Trivia/Trivia'
+import History from './pages/History/History'
 
 function App() {
 
@@ -9,6 +11,8 @@ function App() {
       <h1>Trivia App</h1>
       <Routes>
         <Route path='/' element={<Main />} />
+        <Route path='/trivia/:level_reached' element={<Trivia />} />
+        <Route path='/history' element={<History />}/>
       </Routes>
     </div>
   );
