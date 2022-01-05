ec138967976cc4f31e8633e8092e596bd8ecc1c5
cuervo357@gmail.com <cuervo357@gmail.com>
2022-01-05 15:27:01 -0500

    params

--- challenge-app/src/pages/Trivia/Trivia.jsx
+++ challenge-app/src/pages/Trivia/Trivia.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
 import Modal from '../../components/Modal/Modal'
 import TriviaCard from '../../components/TriviaCard/TriviaCard'
 import { petition } from '../../utils/petitions'
@@ -7,45 +8,53 @@ import { services } from '../../utils/services'
 function Trivia() {
   const [users, setUsers] = useState([])
   const [user, setUser] = useState(null)
-  const [level, setLevel] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [level, setLevel] = useState(null)
   const [final, setFinal] = useState(false)
   const [lose, setLose] = useState(false)
 
+  const { level_reached } = useParams()  
+
   useEffect(async () => { // eslint-disable-line
     setUsers(await petition(services.getUsers.url))
-  }, [])
+  }, [level_reached])
 
   //cargar datos de usuario y llamado a la api del nivel
   useEffect(async () => { // eslint-disable-line
     setUser(users[users.length-1])
-    const petitioLevels = await petition(services.getLevels.url)
-    const currentLevel = await petitioLevels.filter(el => el.level === (users[users.length-1] && users[users.length-1].level_reached))
-    setLevel(currentLevel[0])
-    setIsLoading(false)
+    const petitioLevels = await petition(`${services.getLevels.url}/${level_reached}`)
+    // const currentLevel = await petitioLevels.filter(el => el.level === (users[users.length-1] && users[users.length-1].level_reached))
+    setLevel(await petitioLevels[0])
   }, [users])
 
   return (
     <div>
-      {isLoading ?
-        <p>Cargando...</p>
-        : 
+      <div>
+        <h1>{`Nivel ${level && level.level || '' } `}</h1>
         <div>
-          <h1>{`Nivel ${level && level.level } `}</h1>
-          <div>
-            <span>{`Acumulado: $${user && user.accum}`}</span>
-            <span>{`Premio a ganar: $${level && level.prize.points}`}</span>
-          </div>
-          <TriviaCard level={level} user={user} setUsers={setUsers} setLose={setLose} />
+          <span>{`Acumulado: $${user && user.accum || ''}`}</span>
+          <span>{`Premio a ganar: $${level && level.prize && level.prize.points ||''}`}</span>
         </div>
-      }
+        <TriviaCard level={level} user={user} setUsers={setUsers} setLose={setLose} setFinal={setFinal} />
+      </div>
+
       <button>Retirarse</button>
+
+
+      {/* MODALES */}
       {lose && 
         <Modal 
           title='Qué mal, has perdido...' 
           msg='Lamentablemente te has ido con $0'
           titleBtn='Lo intentaré de nuevo!'
-          user={user}
+          url='/'
+        />
+      }
+      {final &&
+        <Modal 
+          title='FELICIDADES, has ganado!' 
+          msg={`Te llevas el premio mayor de ${user.accum}`}
+          titleBtn='Terminar'
+          url='/'
         />
       }
     </div>
