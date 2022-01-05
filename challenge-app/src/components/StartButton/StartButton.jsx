dfc694d06e0c47564ff25963c681fc44da9bc8f0
cuervo357@gmail.com <cuervo357@gmail.com>
2022-01-05 18:12:41 -0500

    estilos

--- challenge-app/src/components/StartButton/StartButton.jsx
+++ challenge-app/src/components/StartButton/StartButton.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { petition } from '../../utils/petitions'
 import { services } from '../../utils/services'
+import style from './StartButton.module.css'
 
 function StartButton() {
 
@@ -18,7 +19,7 @@ function StartButton() {
   }
 
   return (
-    <div>
+    <div className={style.buttonCtn}>
       <button onClick={createUser}>Comenzar Juego</button>
     </div>
   )
